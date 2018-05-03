1c548f186ade6b1471e2ca007a9c55933e572978
Rawat <www.kudeeprawat@gmail.com>
2018-05-03 16:54:50 +0530

    this is first python program

---
+++ palamdrom.py
@@ -0,0 +1,11 @@
+sum=0;
+rem=0;
+num=int(input("Enter Number which you want to check:"))
+while num>0:
+	rem=num%10;
+	num=num//10;
+	sum=(sum*10)+rem;
+if(num==sum):
+	print("Given no is Palamdrom")
+else:
+	print("Given no is not a Palamdrom")
