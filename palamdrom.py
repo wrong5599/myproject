c688635a97a6a8239094d354bc62962e1ed31d8f
Rawat <www.kudeeprawat@gmail.com>
2018-05-03 17:34:24 +0530

    modifying palamdrom.py file/ add commet

--- palamdrom.py
+++ palamdrom.py
@@ -4,7 +4,7 @@ num=int(input("Enter Number which you want to check:"))
 while num>0:
 	rem=num%10;
 	num=num//10;
-	sum=(sum*10)+rem;
+	sum=(sum*10)+rem;	#this is the comment
 if(num==sum):
 	print("Given no is Palamdrom")
 else:
