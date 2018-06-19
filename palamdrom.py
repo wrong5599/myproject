a2412524a03d5819862926fea05fd9a0aa24fa1f
Rawat <www.kudeeprawat@gmail.com>
2018-06-19 13:15:09 +0530

    added all files to master brach

--- palamdrom.py
+++ palamdrom.py
@@ -1,11 +1,22 @@
 sum=0;
 rem=0;
-num=int(input("Enter Number which you want to check:"))
-while num>0:
-	rem=num%10;
-	num=num//10;
+n=0;
+while True:
+	try:
+		num=input("Enter Number which you want to check:")
+		num=int(num)
+		n=num
+		break
+	except ValueError:
+		print("No valid Integer, Please try again")
+print("Great, you successfuly enter valid Integer")
+while n>0:
+	rem=n%10;
+	n=n//10;
 	sum=(sum*10)+rem;	#this is the comment
 if(num==sum):
+	print(sum);
 	print("Given no is Palamdrom")
 else:
+	print(sum);
 	print("Given no is not a Palamdrom")
